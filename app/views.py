955c9f047d2578852d414da1b43ce06a0b9b8ea1
canaltinoz <canaltinoz0@gmail.com>
2023-11-02 15:25:15 +0300

    .csv files convert to .xlsx

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ import zipfile,subprocess,rarfile,patoolib,shutil,tempfile,pyzipper
 import os
 from django.db import transaction
 from django.core.files import File
+import pandas as pd
 
 def index(request):
     return render(request,'index.html')
@@ -25,9 +26,22 @@ def upload_files(request):
 
             for uploaded_file in uploaded_files:
                 file_path = os.path.join(temp_dir, uploaded_file.name)
+
                 with open(file_path, 'wb') as destination:
                     for chunk in uploaded_file.chunks():
                         destination.write(chunk)
+
+                if uploaded_file.name.endswith('.csv'):
+                    csv_data = pd.read_csv(file_path,encoding='latin1')
+                    xlsx_path = os.path.splitext(file_path)[0] + '.xlsx'
+                    csv_data.to_excel(xlsx_path, index=False,header=True)
+                    os.remove(file_path)
+                
+                else:
+                    final_file_path = os.path.join(temp_dir, uploaded_file.name)
+                    os.rename(file_path, final_file_path)
+
+
                 
             zip_file_path = os.path.join(temp_dir, f'compressed_files_{compressed_file.id}.zip')
             os.chdir(temp_dir)
@@ -70,9 +84,8 @@ def upload_folder(request):
             with open(zip_file_path, 'rb') as zip_file:   
                 compressed_folder.compressed_folder.save(f'compressed_folder_{compressed_folder.id}.zip', File(zip_file))
                 compressed_folder.save()
-
             return redirect(folder_list)
-
+        
         except:
             return redirect(folder_list)
     return render(request, 'upload_folder.html')
